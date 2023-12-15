e85a31ea43a50385e90c693d9fe521168dead207
mukhsia <andre.n.mukhsia@gmail.com>
2023-12-14 17:11:13 -0800

    Created FavoritesController.

---
+++ FullStackAuth_WebAPI/Controllers/ReviewsController.cs
@@ -0,0 +1,55 @@
+﻿using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FullStackAuth_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST api/reviews
+        [HttpPost, Authorize]
+        public IActionResult Post([FromBody] Review review)
+        {
+            try
+            {
+                // Get User Id from JWT Token
+                string userId = User.FindFirstValue("id");
+
+                // Authentication check
+                if (userId == null)
+                {
+                    return Unauthorized();
+                }
+
+                review.UserId = userId;
+
+                _context.Reviews.Add(review);
+                // Invalid model state check
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                _context.SaveChanges();
+
+                return StatusCode(201, review);
+
+            } 
+            catch (Exception ex) 
+            { 
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+    }
+}
