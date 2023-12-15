ecc06252a92da9d19f65757d2e538ea9f4075e3f
mukhsia <andre.n.mukhsia@gmail.com>
2023-12-14 16:33:51 -0800

    Added Favorite Model.

---
+++ FullStackAuth_WebAPI/Migrations/20231215003320_Adding Favorite Model..cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FullStackAuth_WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingFavoriteModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "918c6660-269e-4a49-a407-66fdb41e269a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b7314cf1-eb82-4138-8f4d-bbd8dba4268e");
+
+            migrationBuilder.CreateTable(
+                name: "Favorite",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    BookId = table.Column<string>(type: "longtext", nullable: true),
+                    Title = table.Column<string>(type: "longtext", nullable: true),
+                    ThumbnailUrl = table.Column<string>(type: "longtext", nullable: true),
+                    UserId = table.Column<string>(type: "varchar(255)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favorite", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Favorite_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "29b72429-64de-482b-b938-780f35833356", null, "Admin", "ADMIN" },
+                    { "ce765674-6064-4af8-a852-fd1d8fbf73f3", null, "User", "USER" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favorite_UserId",
+                table: "Favorite",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Favorite");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "29b72429-64de-482b-b938-780f35833356");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ce765674-6064-4af8-a852-fd1d8fbf73f3");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "918c6660-269e-4a49-a407-66fdb41e269a", null, "User", "USER" },
+                    { "b7314cf1-eb82-4138-8f4d-bbd8dba4268e", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}
---
+++ FullStackAuth_WebAPI/Models/Favorite.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Favorite
+    {
+        [Key]
+        public int Id { get; set; }
+        public string BookId { get; set; }
+        public string Title { get; set; }
+        public string ThumbnailUrl { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
