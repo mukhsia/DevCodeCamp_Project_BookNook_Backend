e13d6a5081245cb2fa4a90fccbf4b14f2fbc9a36
mukhsia <andre.n.mukhsia@gmail.com>
2023-12-14 16:40:32 -0800

    Added BookDetailsDto and ReviewWithUserDto.

---
+++ FullStackAuth_WebAPI/DataTransferObjects/BookDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FullStackAuth_WebAPI.DataTransferObjects
+{
+    public class BookDetailsDto
+    {
+        public int Id { get; set; }
+        public string BookId { get; set; }
+        public string Title { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+}
---
+++ FullStackAuth_WebAPI/DataTransferObjects/ReviewWithUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FullStackAuth_WebAPI.DataTransferObjects
+{
+    public class ReviewWithUserDto
+    {
+        public int Id { get; set; }
+        public string BookId { get; set; }
+        public string Text { get; set; }
+        public double Rating { get; set; }
+        public UserForDisplayDto Owner { get; set; }
+    }
+}
